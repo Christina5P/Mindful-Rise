fd0aef81d587bbca317055e3423bc1f831004486
Christina Åhman <christina.ahman@tv4.se>
2024-07-16 21:37:57 +0000

    touch blog index,detail,category html, create superuser

--- blog/views.py
+++ blog/views.py
@@ -12,3 +12,13 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
     paginate_by = 6
+
+def blog_index(request):
+    """
+    Display all blog posts ordered by creation date in descending order.
+    Render from index.html
+    """
+    posts = Post.objects.all().order_by("-created_on")
+    context = {
+        "posts": posts,
+    }

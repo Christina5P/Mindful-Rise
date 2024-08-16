2cbfc4ce1756239db3e678bb0bf834489b9584e4
Christina Åhman <christina.ahman@tv4.se>
2024-08-16 11:39:35 +0000

    restore edit onclick

--- static/js/comments.js
+++ static/js/comments.js
@@ -1,26 +1,35 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const editButtons = document.getElementsByClassName("btn-edit");
-    const commentText = document.getElementById("id_body");
-    const commentForm = document.getElementById("commentForm");
-    const submitButton = document.getElementById("submitButton");
-    const deleteModal = new mdb.Modal(document.getElementById("deleteModal"));
-    const deleteButtons = document.getElementsByClassName("btn-delete");
-    const deleteConfirm = document.getElementById("deleteConfirm");
-
+  
       console.log("everything loaded");
+});
+
+ // Event listeners för edit Buttons
+ const editButtons = document.querySelectorAll('.btn-edit');
+
+ for (let button of editButtons) {
+     button.addEventListener("click", (e) => {
+         let commentId = e.target.getAttribute("data-comment_id") || e.target.closest('button').getAttribute("data-comment_id");
+         let commentContent = document.getElementById(`comment${commentId}`).innerText;
+         commentText.value = commentContent;
+         submitButton.innerText = "Update";
+         const slug = window.slug;
+         commentForm.setAttribute("action", `/post/${slug}/comment/${commentId}/edit/`);
+         
+         console.log("Editing comment ID:", commentId);
+         console.log("Comment content:", commentContent);
+         
+         // Visa edit-formuläret
+         showEditForm(commentId);
+     });
+ }
  
+
     // Event listeners för delete Buttons
     
-    for (let button of deleteButtons) {
+     for (let button of deleteButtons) {
         button.addEventListener("click", (e) => {
             let commentId = e.target.getAttribute("data-comment_id");
-            
-            if (commentId) {
-                deleteConfirm.href = `delete_comment/${commentId}`;
-                deleteModal.show();
-            } else {
-                console.error("Comment ID not found.");
-            }
+	    document.addEventListener("DOMContentLoaded", function() {
+            });
         });
-    }
-});
+    };
